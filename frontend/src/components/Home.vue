584fc2be33c000e4f51e85f491d21369432dc155
benjaminortizulloa <plzbeemyfriend@gmail.com>
2020-11-12 02:27:35 -0600

    added useless toggle and tooltip

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -16,10 +16,26 @@
         </div>
       </div>
       <div
-        style="position: absolute; border: solid black 3px; height: 100%; width: 30%; right: 0; overflow-y: scroll"
+        style="position: absolute; border: solid black 3px; height: 100%; width: 30%; right: 0; "
         ref="sideContainer"
       >
-        <SideBar :info="info" :maxX="maxPct" :width="sideWidth" :height="sideHeight"/>
+        <div height="50px">
+          <label style="margin:25px">Hourly</label>
+          <label class="switch" style="margin: 15px;">
+            <input type="checkbox" v-model="isAnnual">
+            <span class="slider"></span>
+          </label>
+          <label style="margin: 25px">Annually</label>
+        </div>
+        <SideBar
+          :info="info"
+          :maxX="maxPct"
+          :width="sideWidth"
+          :isAnnual="isAnnual"
+          :height="sideHeight-50"
+          :top="50"
+          @setTooltip="setTooltip"
+        />
       </div>
     </div>
   </v-card>
@@ -33,6 +49,7 @@ export default {
   components: { GridSvg, SideBar },
   data: () => ({
     info: [],
+    isAnnual: true,
     maxPct: 0,
     treeWidth: 0,
     treeHeight: 0,
@@ -44,7 +61,10 @@ export default {
       this.info = state.data.data
         .filter(d => d.a_pct90)
         .sort((a, b) => b.a_pct90 - a.a_pct90);
-      this.maxPct = Math.max(...this.info.map(d => d.a_pct90));
+      this.maxPct = {
+        a: Math.max(...this.info.map(d => d.a_pct90)),
+        h: Math.max(...this.info.map(d => d.h_pct90))
+      };
     },
     setTooltip(tooltip) {
       this.$emit("setTooltip", tooltip);
@@ -67,3 +87,56 @@ export default {
   }
 };
 </script>
+<style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgb(184, 28, 163);
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+</style>
+
--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,26 +1,40 @@
 <template>
-  <div v-if="info.length" style="position: absolute; height: 100%; width: 100%;">
+  <div v-if="info.length" :style="divStyle">
     <div v-for="(dta, i) in info" :key="i" class="data">
       <p style="text-align:center;">{{dta.naics_title}}</p>
       <svg height="50px" :width="width">
         <line
-          :x1="xLocation(dta.a_pct10)"
+          :x1="xLocation(dta[`${salaryType}_pct10`])"
           y1="25"
-          :x2="xLocation(dta.a_pct90)"
+          :x2="xLocation(dta[`${salaryType}_pct90`])"
           y2="25"
           stroke-width="3"
           stroke="black"
         ></line>
         <line
-          :x1="xLocation(dta.a_pct25)"
+          :x1="xLocation(dta[`${salaryType}_pct25`])"
           y1="25"
-          :x2="xLocation(dta.a_pct75)"
+          :x2="xLocation(dta[`${salaryType}_pct75`])"
           y2="25"
           stroke-width="11"
           stroke="grey"
         ></line>
-        <circle r="5" cy="25" :cx="xLocation(dta.a_mean)" fill="blue"></circle>
-        <circle r="5" cy="25" :cx="xLocation(dta.a_median)" fill="red"></circle>
+        <circle
+          r="5"
+          cy="25"
+          :cx="xLocation(dta[`${salaryType}_mean`])"
+          fill="blue"
+          @mouseover="mouseOver(`Mean salary for ${dta.naics_title} is ${dta[salaryType + '_mean']}`, $event)"
+          @mouseout="mouseOut"
+        ></circle>
+        <circle
+          r="5"
+          cy="25"
+          :cx="xLocation(dta[`${salaryType}_median`])"
+          fill="red"
+          @mouseover="mouseOver(`Median salary for ${dta.naics_title} is ${dta[salaryType + '_median']}`, $event)"
+          @mouseout="mouseOut"
+        ></circle>
       </svg>
     </div>
   </div>
@@ -31,14 +45,41 @@ import { scaleLinear } from "d3-scale";
 
 export default {
   name: "SideBar",
-  props: ["info", "width", "height", "maxX"],
-  data: () => ({}),
-  computed: {},
+  props: ["info", "width", "height", "maxX", "isAnnual", "top"],
+  data: () => ({
+    divStyle: {
+      position: "absolute",
+      width: "100%",
+      overflowY: "scroll",
+      height: 500 + "px",
+      top: 50 + "px"
+    }
+  }),
+  computed: {
+    salaryType() {
+      if (this.isAnnual) return "a";
+      return "h";
+    }
+  },
+  watch: {
+    height(newVal) {
+      console.log("newVal", newVal);
+      this.$set(this.divStyle, "height", newVal + "px");
+    }
+  },
   methods: {
+    mouseOver(data, event) {
+      let dta = { data: data, event: event, type: "bar" };
+
+      this.$emit("setTooltip", dta);
+    },
+    mouseOut() {
+      this.$emit("setTooltip", { data: null, event: null, type: null });
+    },
     xLocation(d) {
       let that = this;
       let scaleX = scaleLinear()
-        .domain([0, that.maxX])
+        .domain([0, that.maxX[that.salaryType]])
         .range([10, that.width - 10]);
 
       return scaleX(d);
